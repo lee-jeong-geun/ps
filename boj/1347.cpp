97b7edd6f05b7008552df9a10df51726fd2df595
Lee Jeong geun <ljk0411jg@naver.com>
2017-12-21 17:28:53 +0900

    Simulation

--- boj/1347.cpp
+++ boj/1347.cpp
@@ -8,33 +8,39 @@ int arrB[4] = {-1, 0, 1, 0};
 
 int main()
 {
-    tx = 60;
-    ty = 60;
-    dir = 2;
+    //최대 50번 직진 할수있고 상하좌우 다 가능하기 떄문에 맵을 120 by 120으로 잡아 놓고 중간 지점으로
+    //초기화 하였다.
+    tx = 60;    //초기 위치
+    ty = 60;    //초기 위치
+    dir = 2;    //이동방향
     Minx = tx;
     Maxx = tx;
     Miny = ty;
     Maxy = ty;
     scanf("%d", &N);
-    zone[ty][tx] = 1;
+    zone[ty][tx] = 1;   //지나온길은 1로 체크
     for(int i = 0; i < N; i++)
     {
         char move;
         scanf(" %c", &move);
+        //우회전
         if(move == 'R')
         {
             dir = (dir + 1) % 4;
         }
+        //좌회전
         else if(move == 'L')
         {
             dir = (dir + 3) % 4;
         }
+        //직진
         else
         {
             tx += arrA[dir];
             ty += arrB[dir];
         }
         zone[ty][tx] = 1;
+        //출력을 위해서 최소값 최대값 갱신
         Minx = min(Minx, tx);
         Maxx = max(Maxx, tx);
         Miny = min(Miny, ty);
