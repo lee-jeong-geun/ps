615e30f260fc9efc942048a1e219f121eef30b2e
Lee Jeong geun <ljk0411jg@naver.com>
2018-01-14 15:49:44 +0900

    BFS

--- boj/1012.cpp
+++ boj/1012.cpp
@@ -1,81 +1,151 @@
 #include <cstdio>
 #include <iostream>
-#include <queue>
-#include <cstring>
 using namespace std;
 typedef struct _data
 {
-  int x, y;
+    int x, y;
 }Data;
 int M, N, K, zone[55][55], chk[55][55], result;
 int arrA[4] = {0, 0, 1, -1};
 int arrB[4] = {1, -1, 0, 0};
 
-int range(int a, int b)
+/*
+단순 BFS 문제
+맵 전체를 돌면서 1이 있으면 BFS돌려서 싹다 체크해준다.
+BFS시작 할때마다 카운트 해주면 된다.
+*/
+
+//큐 구현
+template<typename T>
+class queue
 {
-  if(a >= 0 && a < M && b >= 0 && b < N)
-  {
-    return 1;
-  }
-  return 0;
-}
+public:
+    class Node
+    {
+    public:
+        T data;
+        Node *link;
+    };
+    int _size;
+    Node *Front, *Rear;
+    queue()
+    {
+        _size = 0;
+        Front = NULL;
+        Rear = NULL;
+    }
+    void push(T data)
+    {
+        _size++;
+        Node *newnode = new Node();
+        newnode->data = data;
+        newnode->link = NULL;
+        if(_size == 1)
+        {
+            Front = newnode;
+            Rear = newnode;
+        }
+        else
+        {
+            Rear->link = newnode;
+            Rear = Rear->link;
+        }
+    }
+    void pop()
+    {
+        _size--;
+        Node *tnode;
+        tnode = Front;
+        Front = Front->link;
+        delete tnode;
+    }
+    T front()
+    {
+        return Front->data;
+    }
+    int size()
+    {
+        return _size;
+    }
+    bool empty()
+    {
+        return _size == 0;
+    }
+};
 
 void init()
 {
-  result = 0;
-  memset(chk, 0, sizeof chk);
-  memset(zone, 0, sizeof zone);
+    for(int i = 0; i < 55; i++)
+    {
+        for(int j = 0; j < 55; j++)
+        {
+            zone[i][j] = 0;
+            chk[i][j] = 0;
+        }
+    }
+    result = 0;
+}
+
+//맵 범위 확인
+bool range(int a, int b)
+{
+    if(a >= 0 && a < M && b >= 0 && b < N)
+    {
+        return 1;
+    }
+    return 0;
 }
 
 void BFS(int x, int y)
 {
-  queue<Data>q;
-  q.push({x, y});
-  while(!q.empty())
-  {
-    Data temp;
-    temp = q.front();
-    q.pop();
-    for(int i = 0; i < 4; i++)
+    queue<Data>q;
+    q.push({x, y});
+    while(!q.empty())
     {
-      Data next;
-      next.x = temp.x + arrA[i];
-      next.y = temp.y + arrB[i];
-      if(range(next.x, next.y) == 1 && zone[next.y][next.x] == 1 && chk[next.y][next.x] == 0)
-      {
-        chk[next.y][next.x] = 1;
-        q.push(next);
-      }
+        Data temp;
+        temp = q.front();
+        q.pop();
+        for(int i = 0; i < 4; i++)
+        {
+            Data next;
+            next.x = temp.x + arrA[i];
+            next.y = temp.y + arrB[i];
+            //1이면 큐에 넣음
+            if(range(next.x, next.y) == 1 && zone[next.y][next.x] == 1 && chk[next.y][next.x] == 0)
+            {
+                chk[next.y][next.x] = 1;
+                q.push(next);
+            }
+        }
     }
-  }
 }
 
 int main()
 {
-  int T;
-  scanf("%d", &T);
-  for(int testcase = 0; testcase < T; testcase++)
-  {
-    init();
-    scanf("%d %d %d", &M, &N, &K);
-    for(int i = 0; i < K; i++)
+    int T;
+    scanf("%d", &T);
+    for(int testcase = 0; testcase < T; testcase++)
     {
-      int x, y;
-      scanf("%d %d", &x, &y);
-      zone[y][x] = 1;
-    }
-    for(int i = 0; i < N; i++)
-    {
-      for(int j = 0; j < M; j++)
-      {
-        if(zone[i][j] == 1 && chk[i][j] == 0)
+        init();
+        scanf("%d %d %d", &M, &N, &K);
+        for(int i = 0; i < K; i++)
         {
-          chk[i][j] = 1;
-          result++;
-          BFS(j, i);
+            int X, Y;
+            scanf("%d %d", &X, &Y);
+            zone[Y][X] = 1;
         }
-      }
+        for(int i = 0; i < N; i++)
+        {
+            for(int j = 0; j < M; j++)
+            {
+                //한번도 안들어갔으면 BFS돌림
+                if(zone[i][j] == 1 && chk[i][j] == 0)
+                {
+                    result++;
+                    BFS(j, i);
+                }
+            }
+        }
+        printf("%d\n", result);
     }
-    printf("%d\n", result);
-  }
 }
