a404cb343e90969e83934797f79aa0b54cc0106c
Lee Jeong geun <ljk0411jg@naver.com>
2018-03-03 18:40:16 +0900

    Prefix Sum

--- boj/11660.cpp
+++ boj/11660.cpp
@@ -1,7 +1,22 @@
 #include <cstdio>
 #include <iostream>
 using namespace std;
-int N, M, zone[1030][1030], Sum[1030][1030];
+int N, M, Sum[1030][1030];
+
+/*
+이 문제는 2차원 행렬에서 구간의 합을 구하는 것인데
+어떤 구간의 합을 원소 하나씩 다 접근하면서 구한다면
+총 1024 * 1024 * 100000의 시간복잡도가 발생한다.
+누적합을 이용해서 어떤 구간의 합을 O(1)로 줄일수 있다.
+1,1부터 100,100까지의 합을 구하고 싶을때
+Num[1][1] + Num[1][2] + ... + Num[100][99] + Num[100][100]처럼 일일이 구할수 있지만
+Sum[100][100]에다 위에 식을 저장하면 한번에 구할수 있다.
+곧 Sum[100][100]은 1,1부터 100,100까지의 합을 저장한 값이다.
+5,5부터 65,40의 합을 구하고 싶으면
+Sum[65][40] - Sum[4][40] - Sum[65][4] + Sum[4][4]를 하면된다.
+*/
+
+
 
 int main()
 {
@@ -10,21 +25,15 @@ int main()
     {
         for(int j = 1; j <= N; j++)
         {
-            scanf("%d", &zone[i][j]);
+            int num;
+            scanf("%d", &num);
+            Sum[i][j] = Sum[i - 1][j] + Sum[i][j - 1] - Sum[i - 1][j - 1] + num;
         }
     }
-    for(int i = 1; i <= N; i++)
+    for(int j = 0; j < M; j++)
     {
-        for(int j = 1; j <= N; j++)
-        {
-            Sum[i][j] = Sum[i - 1][j] + Sum[i][j - 1] + zone[i][j] - Sum[i - 1][j - 1];
-        }
+        int sx, sy, ex, ey;
+        scanf("%d %d %d %d", &sy, &sx, &ey, &ex);
+        printf("%d\n", Sum[ey][ex] - Sum[sy - 1][ex] - Sum[ey][sx - 1] + Sum[sy - 1][sx - 1]);
     }
-    for(int i = 0; i < M; i++)
-    {
-        int x1, y1, x2, y2;
-        scanf("%d %d %d %d", &y1, &x1, &y2, &x2);
-        printf("%d\n", Sum[y2][x2] - Sum[y2][x1 - 1] - Sum[y1 - 1][x2] + Sum[y1 - 1][x1 - 1]);
-    }
-    
 }
