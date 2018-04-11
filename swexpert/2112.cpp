e21993d19b694fca588d29dba8b1b18cecc1caf9
Lee Jeong geun <ljk0411jg@naver.com>
2018-04-11 20:18:00 +0900

    Brute Force

--- swexpert/2112.cpp
+++ swexpert/2112.cpp
@@ -4,6 +4,11 @@
 using namespace std;
 int D, W, K, zone[15][25], result = 987654321;
 
+/*
+각 행에 대하여 나올수 있는 가지 수는 3개이고 총 13개의 행이라서
+모든 경우의 수는 3 ^ 13개 이다. 가지치기를 이용해서 경우의 수를 확 줄일수 있다.
+*/
+
 int schk(int x)
 {
     int count = 1, temp = zone[0][x];
@@ -90,4 +95,104 @@ int main()
         func(0, 0);
         printf("#%d %d\n", testcase, result);
     }
+}
+
+
+
+
+//나중에 푼 것
+
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int D, W, K, zone[25][25], result;
+
+//성능검사
+bool funcchk()
+{
+	if (K == 1)
+	{
+		return 1;
+	}
+	for (int i = 0; i < W; i++)
+	{
+		int count = 1, flag = 0;
+		for (int j = 0; j < D - 1; j++)
+		{
+			if (zone[j][i] == zone[j + 1][i])
+			{
+				count++;
+				if (count >= K)
+				{
+					flag = 1;
+					break;
+				}
+			}
+			else
+			{
+				count = 1;
+			}
+		}
+		if (flag == 0)
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+
+void func(int depth, int count)
+{
+    //현재 바꾼 횟수가 결과보다 크면 더이상 볼 필요 없음
+	if (count >= result)
+	{
+		return;
+	}
+	int temp[25];
+	if (depth == D)
+	{
+		if (funcchk() == 1)
+		{
+			result = min(result, count);
+		}
+		return;
+	}
+	func(depth + 1, count);
+	for (int i = 0; i < W; i++)
+	{
+		temp[i] = zone[depth][i];
+		zone[depth][i] = 1;
+	}
+	func(depth + 1, count + 1);
+	for (int i = 0; i < W; i++)
+	{
+		zone[depth][i] = 0;
+	}
+	func(depth + 1, count + 1);
+	for (int i = 0; i < W; i++)
+	{
+		zone[depth][i] = temp[i];
+	}
+
+}
+
+int main()
+{
+	int T;
+	scanf("%d", &T);
+	for (int testcase = 1; testcase <= T; testcase++)
+	{
+		result = 987654321;
+		scanf("%d %d %d", &D, &W, &K);
+		for (int i = 0; i < D; i++)
+		{
+			for (int j = 0; j < W; j++)
+			{
+				scanf("%d", &zone[i][j]);
+			}
+		}
+		func(0, 0);
+		printf("#%d %d\n", testcase, result);
+	}
 }
