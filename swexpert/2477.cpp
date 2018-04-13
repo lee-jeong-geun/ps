c8d33ebf207bcb8f5b84f3bc316e13ed03ff09fe
Lee Jeong geun <ljk0411jg@naver.com>
2018-04-14 01:21:29 +0900

    Simulation

--- swexpert/2477.cpp
+++ swexpert/2477.cpp
@@ -12,6 +12,12 @@ typedef struct _data2
 Data reception[15], repair[15];
 int N, M, K, A, B, Human[1005], Time = 0, result;
 
+/*
+주어진 조건에 맞게 시뮬레이션 돌리면 된다.
+접수창에 도착 하면 큐에 넣어주고 접수창이 비어있으면 큐에서 빼서 접수창에 넣는다.
+접수 완료하면 큐에 넣어주고 수리창이 비어있으면 큐에서 빼서 수리창에 넣는다.
+*/
+
 template<typename T>
 class queue
 {
@@ -84,24 +90,30 @@ void init()
     result = 0;
 }
 
+//접수창 구현
 void receptfunc()
 {
     for(int i = 1; i <= N; i++)
     {
+        //접수창 안비었을 경우
         if(reception[i].afterTime > 0)
         {
             reception[i].afterTime--;
+            //작업 완료
             if(reception[i].afterTime == 0)
             {
                 Data2 Push;
                 Push.humanidx = reception[i].humanidx;
                 Push.receptnum = i;
                 reception[i].humanidx = -1;
+                //수리창으로 가는 큐에 푸쉬
                 q2.push(Push);
             }            
         }
+        //접수창 비었을 경우
         else
         {
+            //접수창으로 가는 큐에 데이터가 있을 경우
             if(q1.size() > 0)
             {
                 Data2 temp;
@@ -122,6 +134,7 @@ void receptfunc()
     }
 }
 
+//수리창 구현
 void repairfunc()
 {
     for(int i = 1; i <= M; i++)
@@ -148,6 +161,7 @@ void repairfunc()
     }
 }
 
+//시뮬레이션
 void func()
 {
     Time = Human[1];
@@ -155,6 +169,7 @@ void func()
     {
         for(int j = i; j <= K; j++)
         {
+            //차량 정비소에 도착했을시
             if(Time == Human[j])
             {
                 i = j + 1;
@@ -219,4 +234,159 @@ int main()
         }
         printf("#%d %d\n", testcase, result);
     }
+}
+
+
+//나중에 푼것
+
+#include <cstdio>
+#include <iostream>
+#include <queue>
+using namespace std;
+typedef struct _data
+{
+	int idx;
+	int before;
+}Data;
+typedef struct _data2
+{
+	int idx;
+	int Time;
+}Data2;
+Data2 Receptarr[15], Repairarr[15];
+int N, M, K, A, B, Recept[15], Repair[15], Human[1005], result;
+
+void func()
+{
+	int Time, Count = 0;
+	Time = -1;
+	queue<int>waitq;
+	queue<Data>waitq2;
+	for (int i = 0; i < N; i++)
+	{
+		Receptarr[i].idx = -1;
+	}
+	for (int i = 0; i < M; i++)
+	{
+		Repairarr[i].idx = -1;
+	}
+	for (int i = 0; ;)
+	{
+		Time++;
+		if (Count == K)
+		{
+			break;
+		}
+		for (int j = i; j < K; j++)
+		{
+            //차량 정비소에 도착시
+			if (Human[j] == Time)
+			{
+				waitq.push(j);
+				i++;
+			}
+			else
+			{
+				break;
+			}
+		}
+        //수리창 작업 완료 된것들 빼줌
+		for (int j = 0; j < M; j++)
+		{
+			if (Repairarr[j].idx != -1 && Repairarr[j].Time == Time)
+			{
+				Repairarr[j].idx = -1;
+				Count++;
+			}
+		}
+        //접수창 작업 완료 된것들 빼줌
+		for (int j = 0; j < N; j++)
+		{
+			if (Receptarr[j].idx != -1 && Receptarr[j].Time == Time)
+			{
+                //수리창으로 가는 큐에 푸쉬
+				waitq2.push({ Receptarr[j].idx, j });
+				Receptarr[j].idx = -1;
+			}
+		}
+		while (!waitq.empty())
+		{
+			int flag = 0;
+			for (int j = 0; j < N; j++)
+			{
+                //접수창 비었을 경우
+				if (Receptarr[j].idx == -1)
+				{
+					int temp;
+					temp = waitq.front();
+					waitq.pop();
+					Receptarr[j].idx = temp;
+					Receptarr[j].Time = Time + Recept[j];
+					flag = 1;
+					break;
+				}
+			}
+			if (flag == 0)
+			{
+				break;
+			}
+		}
+		while (!waitq2.empty())
+		{
+			int flag = 0;
+			for (int j = 0; j < M; j++)
+			{
+                //수리창 비었을 경우
+				if (Repairarr[j].idx == -1)
+				{
+					Data temp;
+					temp = waitq2.front();
+					waitq2.pop();
+					Repairarr[j].idx = temp.idx;
+					Repairarr[j].Time = Time + Repair[j];
+					flag = 1;
+					if (temp.before == A && j == B)
+					{
+						result += temp.idx + 1;
+					}
+					break;
+				}
+			}
+			if (flag == 0)
+			{
+				break;
+			}
+		}
+	}
+}
+
+int main()
+{
+	int T;
+	scanf("%d", &T);
+	for (int testcase = 1; testcase <= T; testcase++)
+	{
+		result = 0;
+		scanf("%d %d %d %d %d", &N, &M, &K, &A, &B);
+		A--;
+		B--;
+		for (int i = 0; i < N; i++)
+		{
+			scanf("%d", &Recept[i]);
+		}
+		for (int i = 0; i < M; i++)
+		{
+			scanf("%d", &Repair[i]);
+		}
+		for (int i = 0; i < K; i++)
+		{
+			scanf("%d", &Human[i]);
+		}
+		func();
+		if (result == 0)
+		{
+			result = -1;
+		}
+		printf("#%d %d\n", testcase, result);
+	}
 }
