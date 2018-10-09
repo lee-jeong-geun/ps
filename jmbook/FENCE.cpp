77337aa367740e68328aeb6d6f52df69349e3c92
Lee Jeong geun <ljk0411jg@naver.com>
2018-10-09 16:51:15 +0900

    Stack, Divide and Conquer

--- jmbook/FENCE.cpp
+++ jmbook/FENCE.cpp
@@ -1,3 +1,57 @@
+//divide and conquer
+
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int N, fence[20005];
+
+int func(int left, int right)
+{
+    if(left == right)
+    {
+        return fence[left];
+    }
+    int mid, ret = 0, lidx, ridx, minheight = 987654321;
+    mid = (left + right) / 2;
+    lidx = mid;
+    ridx = mid;
+    ret = max(func(left, mid), func(mid + 1, right));
+    while(left <= lidx && ridx <= right)
+    {
+        if(fence[lidx - 1] < fence[ridx + 1])
+        {
+            ridx++;
+        }
+        else
+        {
+            lidx--;
+        }
+        minheight = min(minheight, min(fence[lidx], fence[ridx]));
+        ret = max(ret, (ridx - lidx + 1) * minheight);
+    }
+    return ret;
+}
+
+int main()
+{
+    int C;
+    scanf("%d", &C);
+    for(int testcase = 0; testcase < C; testcase++)
+    {
+        scanf("%d", &N);
+        for(int i = 1; i <= N; i++)
+        {
+            scanf("%d", &fence[i]);
+        }
+        fence[0] = -1;
+        fence[N + 1] = -1;
+        printf("%d\n", func(1, N));
+    }
+}
+
+//stack
+
 #include <cstdio>
 #include <iostream>
 #include <algorithm>
