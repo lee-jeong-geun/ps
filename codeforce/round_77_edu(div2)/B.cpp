42f0ae60ffa67b05c4c832a3d4e3d0e7d6df9e9b
lee-jeong-geun <ljk0411jg@naver.com>
2019-11-29 05:04:02 +0900

    Math

---
+++ codeforce/round_77_edu(div2)/B.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int a, b, result;
+
+/*
+주어진 연산 a를 1깎고 b를 2깎거나 a를 2깎고 b를 1깎아서 a, b둘다 0으로 만들어야 한다.
+둘다 0으로 만들기 위해서는 a + b가 3의 배수여야 한다. 그 이유는 2가지 연산 모두
+한번 실행할 때마다 a와 b에 대해서 총 3을 깎기 때문에 a + b가 3의 배수가 아니라면
+절대 0, 0으로 만들수 없다.
+a + b가 3의 배수일 떄 
+*/
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    for(int tc = 0; tc < t; tc++)
+    {
+        result = 0;
+        scanf("%d %d", &a, &b);
+        if((a + b) % 3 == 0 && min(a, b) * 2 >= max(a, b))
+            result = 1;
+        if(result == 1)
+            printf("YES\n");
+        else
+            printf("NO\n");
+    }
+}
